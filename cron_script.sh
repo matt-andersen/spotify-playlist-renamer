0f05bbf3159a7eb10ff2d52072549324a285ef8e
Matt Andersen <mattandersen0@gmail.com>
2021-12-12 20:53:26 +1000

    Add logging and cron script

---
+++ cron_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /opt/spotify-playlist-renamer/venv/bin/activate
+cd /opt/spotify-playlist-renamer
+python3.6 main.py
+deactivate
--- main.py
+++ main.py
@@ -11,25 +11,34 @@ SCOPE = "playlist-modify-public playlist-modify-private"
 PLAYLIST_ID = "5dZCTRD5inv8lWdVzCmVYb"
 PLAYLIST_NAME = "Dad Rock Essentials"
 
-logger = logging.getLogger("examples.change_playlist_details")
-logging.basicConfig(level="INFO")
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setFormatter(
+    logging.Formatter("[%(asctime)s] %(levelname)s: in %(filename)s: %(message)s")
+)
+logger.addHandler(handler)
 
 
 def main():
-    sp = spotipy.Spotify(
-        auth_manager=SpotifyOAuth(
-            scope=SCOPE,
-            client_id=os.environ.get("CLIENT_ID"),
-            client_secret=os.environ.get("CLIENT_SECRET"),
-            redirect_uri=os.environ.get("REDIRECT_URI"),
+    while True:
+
+        logger.info("Running job")
+
+        sp = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(
+                scope=SCOPE,
+                client_id=os.environ.get("CLIENT_ID"),
+                client_secret=os.environ.get("CLIENT_SECRET"),
+                redirect_uri=os.environ.get("REDIRECT_URI"),
+            )
         )
-    )
 
-    playlist_name = sp.playlist(PLAYLIST_ID)["name"]
+        playlist_name = sp.playlist(PLAYLIST_ID)["name"]
 
-    if playlist_name != PLAYLIST_NAME:
-        logger.info(f"Renaming playlist back to {PLAYLIST_NAME}")
-        sp.playlist_change_details(playlist_id=PLAYLIST_ID, name=PLAYLIST_NAME)
+        if playlist_name != PLAYLIST_NAME:
+            logger.info(f"Renaming playlist back to {PLAYLIST_NAME}")
+            sp.playlist_change_details(playlist_id=PLAYLIST_ID, name=PLAYLIST_NAME)
 
 
 if __name__ == "__main__":
